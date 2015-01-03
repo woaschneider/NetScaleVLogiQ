a6931d5619d1545db2af5edf63b0a003900f200a
Wolfgang Schneider <w.schneider@hamburger-waagenbau.de>
2015-01-03 17:56:14 +0100

    zw

---
+++ Netscale Business Object EF/Adressen/AdressenRules.Partial.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+
+using OakLeaf.MM.Main;
+using OakLeaf.MM.Main.Business;
+using OakLeaf.MM.Main.Collections;
+
+namespace HWB.NETSCALE.BOEF
+{
+	/// <summary>
+	/// Summary description for AdressenRules.
+	/// </summary>
+	public partial class AdressenRules
+	{
+		/// <summary>
+		/// Checks business rules against the specified entity
+		/// </summary>
+		/// <param name="entity">Entity</param>
+		public override void CheckExtendedRulesHook<EntityType>(EntityType entity)
+		{
+			AdressenEntity currentEntity = entity as AdressenEntity;
+			
+			// Call Validation methods
+
+		}
+	}
+}
